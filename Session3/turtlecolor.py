ea330050355536e47b10adaa9a9be85725527f46
DESKTOP-KBSLCQ1\Windows 10 TIMT <dieforice@gmail.com>
2017-07-30 18:17:04 +0700

    homework session 3

--- Session3/turtlecolor.py
+++ Session3/turtlecolor.py
@@ -1,9 +1,9 @@
 from turtle import *
 
-colors=["blue","red","grey","red","blue","brown","yellow","grey"]
+colors=["red","blue","brown","yellow","grey"]
 for p in range(7,1,-1):
     for i in range(p):
-        color(colors[p])
+        color(colors[p-3])
         forward(100)
         left(360/p)
         
